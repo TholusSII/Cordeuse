0366a2b80709fec8f7130c9af9bec7db9279a319
TholusSII <thomas.lusseau@laposte.net>
2026-08-04 21:14:49 +0200

    Lie les bibliothèques locales au sketch Arduino SP55

---
+++ Arduino/CordeuseSP55/SP55Libraries.cpp
@@ -0,0 +1,5 @@
+// Le constructeur Arduino ne compile normalement que les sources du dossier
+// du sketch. Ce fichier rattache explicitement les implémentations locales
+// conservées dans le dépôt, sans dupliquer leur code.
+#include "../PID_v1.cpp"
+#include "../HX711.cpp"
